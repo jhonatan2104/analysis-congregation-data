64c831df9de38bbb7ffa0d177a1f9bf3e0396f11
jhonatan2104 <xdjhonatan0@gmail.com>
2021-02-12 15:03:14 -0300

    feat: send email error

--- src/bricks/mail.py
+++ src/bricks/mail.py
@@ -29,8 +29,46 @@ def mail(path_doc):
     part = MIMEBase('application', "octet-stream")
     part.set_payload(attachment.read())
     encoders.encode_base64(part)
+
+    name_sheet = app_config['NAME_SHEET']
+
     part.add_header('Content-Disposition',
-                    'attachment; filename="WorkBook3.xlsx"')
+                    f'attachment; filename="{name_sheet}.xlsx"')
+
+    msg.attach(part)
+    text = msg.as_string()
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(email_from, email_password)
+
+    server.sendmail(email_from, email_to, text)
+    server.quit()
+
+
+def message(path_doc, body, subject, name_sheet):
+
+    app_config = config()
+
+    email_from = app_config['MAIL']['EMAIL_FROM']
+    email_to = app_config['MAIL']['EMAIL_TO']
+    email_password = app_config['MAIL']['EMAIL_PASSWORD']
+
+    msg = MIMEMultipart()
+    msg['From'] = email_from
+    msg['To'] = email_to
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    filename = path_doc
+    attachment = open(filename, 'rb')
+
+    part = MIMEBase('application', "octet-stream")
+    part.set_payload(attachment.read())
+    encoders.encode_base64(part)
+
+    part.add_header('Content-Disposition',
+                    f'attachment; filename="{name_sheet}.xlsx"')
 
     msg.attach(part)
     text = msg.as_string()
--- src/bricks/validation.py
+++ src/bricks/validation.py
@@ -1,10 +1,16 @@
 import sys
 from ..config.config import config
+from ..bricks.mail import message
 
 
 def validation(df):
 
     app_config = config()
+
+    # Config message erro
+    name_congregation = app_config['NAME_CONGREGATION']
+    name_sheet = f'Erro-{name_congregation}'
+    body = 'Estamos enviando em anexo a planilia em um formato que te ajude a encontrar o erro'
 
     destination_folder = app_config['DESTINATION_FOLDER']
     name_coll_service = app_config['COLUMNS']['NAME_COLL_SERVICE']
@@ -14,9 +20,21 @@ def validation(df):
     df_erro_name_repeated = df.loc[erro_name_repeated > 1, :]
     if len(df_erro_name_repeated) > 0:
         print("Erro: name repeated")
+
+        path_doc = f"{destination_folder}/NOME_REPETIDO.xlsx"
+
+        subject = 'Algo deu errado: Existe nomes repetidos na sua planilia'
+
         df_erro_name_repeated.to_excel(
-            f"{destination_folder}/NOME_REPETIDO.xlsx",
+            path_doc,
             sheet_name='Erro'
+        )
+
+        message(
+            path_doc,
+            body,
+            subject,
+            name_sheet
         )
         sys.exit()
 
@@ -27,10 +45,22 @@ def validation(df):
     ]
     if len(erro_col_service_privilege) > 0:
         print("Erro: column service privilege")
+
+        path_doc = f"{destination_folder}/DADOS_INVÁLIDOS_PRIVILÉGIOS_SERVICO.xlsx"
+        subject = f'Algo deu errado: Tipos inválido de {name_coll_service}'
+
         erro_col_service_privilege.to_excel(
             f"{destination_folder}/DADOS_INVÁLIDOS_PRIVILÉGIOS_SERVICO.xlsx",
             sheet_name='Erro'
         )
+
+        message(
+            path_doc,
+            body,
+            subject,
+            name_sheet
+        )
+
         sys.exit()
 
     # Verificando Erros coluna Privilégio ministerial
@@ -39,9 +69,21 @@ def validation(df):
     ]
     if len(erro_col_service_ministerial) > 0:
         print("Erro: column service ministerial")
+
+        path_doc = f"{destination_folder}/DADOS_INVÁLIDOS_PRIVILÉGIOS_MINISTERIAL.xlsx"
+        subject = f'Algo deu errado: Tipos inválido de {name_coll_ministerial}'
+
         erro_col_service_ministerial.to_excel(
             f"{destination_folder}/DADOS_INVÁLIDOS_PRIVILÉGIOS_MINISTERIAL.xlsx",
             sheet_name='Erro')
+
+        message(
+            path_doc,
+            body,
+            subject,
+            name_sheet
+        )
+
         sys.exit()
 
     return df
